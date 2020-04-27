7796d06ee42ffa6227215a12d6c1822b302beae0
Christian Toledo <christian.odt@gmail.com>
2020-04-27 13:42:43 -0300

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
   async function handleAddRepository() {
     // TODO
   }
@@ -16,10 +19,7 @@ function App() {
       <ul data-testid="repository-list">
         <li>
           Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
+          <button onClick={() => handleRemoveRepository(1)}>Remover</button>
         </li>
       </ul>
 
