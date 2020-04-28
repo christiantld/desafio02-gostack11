b3f5e4310f5832ebed33055c96e0881f0baffc1f
Christian Toledo <christian.odt@gmail.com>
2020-04-28 17:05:45 -0300

    add useEffect

--- src/App.js
+++ src/App.js
@@ -6,21 +6,39 @@ import "./styles.css";
 function App() {
   const [repositories, setRepositories] = useState([]);
 
+  //carregar os repositorios quando carregar o componente
+  useEffect(() => {
+    async function loadRepos() {
+      const response = await api.get("/repositories");
+
+      setRepositories(response.data);
+    }
+    loadRepos();
+  }, [repositories]);
+
   async function handleAddRepository() {
     // TODO
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    const response = await api.get("/repositories");
+
+    setRepositories(response.data);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-          <button onClick={() => handleRemoveRepository(1)}>Remover</button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.name}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
